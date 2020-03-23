86787ec4f98a0f351642c94a5ac8f3bac3b526fc
torates <goddengit5@gmail.com>
2020-03-23 12:44:14 +0100

    Added img database, bot now sends random img on call. Fixed the commands.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
 
-# NOTE: Commands are not working, for some reason
-# they aren't functioning. 22/03/2020
 @client.command()
 async def test(ctx, *arg):
     await ctx.send('{}'.format(' '.join(arg)))
@@ -16,6 +14,17 @@ async def ex(ctx, arg):
         print('exiting')
         await ctx.send('ok me voy ps... mal amigo')
         await client.close()
+
+@client.group()
+async def luisp(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('comando invalido...')
+
+@luisp.command()
+async def img(ctx):
+    image_to_send = str(random.randrange(1,9))
+    print(image_to_send)
+    await ctx.send(file=discord.File("C:\\Users\\misu1\\OneDrive\\Documents\\luispiresbot\\image_database\\{}.jpeg".format(image_to_send)))
 
 
 @client.event
@@ -44,25 +53,26 @@ async def on_message_delete(message):
 
 @client.event
 async def on_message(message):
-    culprit = message.author.name
+    culprit = message.author.id
     praises = [ 'HA HABLADO EL DIOS {0.author.mention} pires... el demonio'.format(message),
-                'alabado sea {0.author.mention} pires... el monsenor robesto sipols'.format(message),
-                'las cartas de {0.author.mention} segun san jose... verisuclo 9 cap 1'.format(message),
-                'silaba tonica',
-                '*trompetas* cabron!!!! respeta!!! luis p esta abland....',
-                '{0.author.mention} elohim essaim eleison kyrie'.format(message),
-                'hecho x andres',
-                '{0.author.mention} pires the creator... igor'.format(message),
-                'jajaajj q dies este idiota {0.author.mention}????'.format(message),
-                'agh... coronavirus coronavirus... lavese las manos',
-                'IGNIS SANCTUS Fuego eterno',
-                'ok toy ladiyao'
+                    'alabado sea {0.author.mention} pires... el monsenor robesto sipols'.format(message),
+                    'las cartas de {0.author.mention} segun san jose... verisuclo 9 cap 1'.format(message),
+                    'silaba tonica',
+                    '*trompetas* cabron!!!! respeta!!! luis p esta abland....',
+                    '{0.author.mention} elohim essaim eleison kyrie'.format(message),
+                    'hecho x andres',
+                    '{0.author.mention} pires the creator... igor'.format(message),
+                    'jajaajj q dies este idiota {0.author.mention}????'.format(message),
+                    'agh... coronavirus coronavirus... lavese las manos',
+                    'IGNIS SANCTUS Fuego eterno',
+                    'ok toy ladiyao'
     ]
-    if culprit == 'FlamingMilhouse' or culprit == 'Hola me llamo paez':
+    if culprit == '691249275023261736':
         await message.channel.send(random.choice(praises))
     else:
         pass
+    await client.process_commands(message)
 
 
 
-client.run('NjkxMjQ5Mjc1MDIzMjYxNzM2.XndSMw.iqLk2iZtUSF2SgDTG06tXIN6TS8')
+client.run('NjkxMjQ5Mjc1MDIzMjYxNzM2.XniOyg.N9ut-ozzd5closr0TQf1Tfb7R7w')
